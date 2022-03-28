298a57ada62c6c3fbf4ffff3cb03ca5c84b11211
hyo-max <yula1147@yahoo.com>
2022-03-28 10:17:09 +0900

    220328

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,55 @@
 /*   By: hyojpark <hyojpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 21:16:55 by hyojpark          #+#    #+#             */
-/*   Updated: 2022/03/22 19:57:58 by hyojpark         ###   ########.fr       */
+/*   Updated: 2022/03/27 23:37:04 by hyojpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*read_line(int fd, char *save)
+{
+	char *buf
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return(NULL);
+	while(buf)
+	{
+		read(fd, buf, BUFFER_SIZE);
+	}
+
+	return (save);
+}
+
 char	*get_next_line(int fd)
 {
-	char *buf;
+	static char	*save;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf)
-		return (NULL);
+	save = read_line(fd, save);
 
-		
-	read(fd,buf,BUFFER_SIZE);
+
+	file_bite = read(fd, buf, BUFFER_SIZE);
+	if (file_bite == -1)
+	{
+		free(buf);
+		return (NULL);
+	}
+	i = 0;
+	while (file_bite)
+	{
+		if (buf[i] != '\n')
+		{
+			buf[i];
+		}
+	}
+	buf[i] = '\0';
+
+	return (buf);
 	//read()
 	//read, malloc, free
 	///파일 디스크립터가 파일의 주민번호
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hyojpark <hyojpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 17:12:51 by hyojpark          #+#    #+#             */
-/*   Updated: 2022/03/22 18:02:40 by hyojpark         ###   ########.fr       */
+/*   Updated: 2022/03/25 19:58:17 by hyojpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,67 @@
 
 int	main(void)
 {
-	// char buffer[20];
+//gnl_test
+	int	fd;
+	char *line;
 
-	// FILE *fp = fopen("hello.txt", "r");
-	// fgets(buffer,sizeof(buffer),fp);
-	// printf("%s\n",buffer);
-	// fclose(fp);
+	fd = open("hello.txt", O_RDONLY);
+	if(fd < 0)
+		printf("file open error");
+	// while ((line = get_next_line(fd)))
+	// {
+	// 	printf("%s\n",line);
+	// 	free(line);
+	// }
+	line = get_next_line(fd);
+	printf("%s\n",line);
+	free(line);
+	close(fd);
+	
 
-	// return (0);
+
+//파일 오픈하는 법
+	// int fd;
+	// char buff[42];
+	// int	file_bite;
+	// int i = 0;
+
+	// fd = open("hello.txt", O_RDONLY);
+	// if (fd < 0)
+	// 	printf("error file open");
+	// else
+	// {
+	// 	file_bite = read(fd, buff, sizeof(buff));
+	// 	if (file_bite > 0)
+	// 	{
+	// 		// printf("%d\n", file_bite);
+	// 	}
+	// 	i = 0;
+	// 	while(file_bite){
+	// 		write(1, &buff[i], 1);
+	// 		i++;
+	// 		file_bite--;
+	// 	}
+	// 	// printf("%s\n", buff);
+	// 	close(fd);
+	// }
+
+//파일디스크립터 값 조회
+	// int fd_1;
+    // int fd_2;
+    
+    // fd_1 = open("a.out", O_RDONLY);
+    // printf("fd_1 = %d\n", fd_1);
+    // fd_2 = open("a.out", O_RDONLY);
+    // printf("fd_2 = %d\n", fd_2);
+
+	// int fd_1;
+    // int fd_2;
+    
+    // fd_1 = open("a.out", O_RDONLY);
+    // printf("fd_1 = %d\n", fd_1);
+    // close(fd_1);
+    // fd_2 = open("a.out", O_RDONLY);
+    // printf("fd_2 = %d\n", fd_2);
+    // close(fd_2);
 }
