363f81d8651ccc0bdcd8e75439e27de541b4099c
hyo-max <yula1147@yahoo.com>
2022-03-29 09:50:23 +0900

    220329

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hyojpark <hyojpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 21:16:55 by hyojpark          #+#    #+#             */
-/*   Updated: 2022/03/28 19:51:09 by hyojpark         ###   ########.fr       */
+/*   Updated: 2022/03/29 09:38:44 by hyojpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 
 char	*buf_save(char *buf_backup)
 {
-	int	i;
-
-	i = 0;
-	while(buf_backup != '\0')
 
 	return (buf_backup);
 }
@@ -25,8 +21,12 @@ char	*buf_save(char *buf_backup)
 char	*make_line(char *buf_backup)
 {
 	char	*line;
+	int	i;
 
-
+	i = 0;
+	if (!buf_backup[i])
+		return (NULL);
+	while(buf_backup != '\0')
 
 	
 	return (line);
